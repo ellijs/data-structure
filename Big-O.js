78ed48234209fa944d79e4c60cf200386c56830f
ellijs <ellijs@hotmail.com>
2021-11-18 03:10:50 -0500

    Big O Notation

---
+++ Big-O.js
@@ -0,0 +1,59 @@
+//Linear Time
+
+const data = ['A', 'B', 'C', 'D']
+const data2 = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < data2.length; i++ ) {
+    console.log(data[j] + data2[i])
+}
+
+
+const data3 = ['A', 'B', 'C']
+const out = []
+
+for (let j= 0; j < data3.length; j++ ) {
+    out[i] = data[i]
+}
+
+O(n)
+
+
+
+
+// Quadratic Time - ex) bubble sort, selection sort, insertion sort
+// Nested Loops
+
+for (let j= 0; j < data.length; j++ ) {
+    for (let i = 0; i < data2.length; i++ ) {
+        console.log(data[j] + data2[i])
+        console.log(data[j] + data2[i])
+        console.log(data[j] + data2[i])
+        console.log(data[j] + data2[i])
+    }
+}
+
+O(n^2)
+// remove constant
+// O(n^2 + n) ===> O(n^2)
+
+
+
+
+
+// Constant Time - ex) Hash tables
+
+const pizza = { 
+    'supreme' : 10, 
+    'pepperoni' : 8,
+    'hawaiian' : 14
+}
+
+O(1)
+
+
+
+
+// Logarithmic Time 
+// Can't use binary search. Faster than above
+
+O(log n)
